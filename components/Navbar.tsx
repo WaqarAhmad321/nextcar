bf58a5fb8172167a113a3de5c797dbdf4e297944
Waqar Ahmad <codewithwaqarahmad@gmail.com>
2023-10-25 08:39:39 -0700

    feat: customized buttons and navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
+import { CustomButton } from "./index";
+
 const Navbar = () => {
   return (
     <header className="w-full absolute z-10">
-      <nav ></nav>
+      <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 py-4">
+        <Link href="/">
+          <Image
+            src="/logo.svg"
+            alt="logo"
+            width={118}
+            height={18}
+            className="object-contain"
+          />
+        </Link>
+
+        <CustomButton
+          title="Sign in"
+          btnType="button"
+          containerStyles="text-primary-blue rounded-full bg-white min-w-[130px]"
+        />
+      </nav>
     </header>
   );
 };
