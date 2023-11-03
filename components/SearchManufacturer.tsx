95b296992d4441178fe1af9999e3b63b2b495941
Waqar Ahmad <codewithwaqarahmad@gmail.com>
2023-11-02 23:08:00 -0700

    feat: quotes added

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -59,7 +59,7 @@ const SearchManufacturer = ({
                 <Combobox.Option
                   value={query}
                   className="search-manufacturer__option">
-                  Create "{query}"
+                  Create &quot;{query}&quot;
                 </Combobox.Option>
               ) : (
                 filteredManufacturers.map((item) => (
